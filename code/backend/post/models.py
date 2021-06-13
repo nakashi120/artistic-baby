0d36dba75770d760841aa70790e98e42abdfa3d2
nakashi120 <nakashi120@gmail.com>
2021-06-14 01:47:14 +0900

    just commit

--- code/backend/post/models.py
+++ code/backend/post/models.py
@@ -12,4 +12,4 @@ class PostModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(verbose_name='タイトル', max_length=30)
     content = models.TextField()
-    
+    author = models.CharField(verbose_name="投稿者", max_length=30)
