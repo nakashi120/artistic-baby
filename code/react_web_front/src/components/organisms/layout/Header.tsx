7ae96942dd6658c14e4338e383338f73af264607
nakashi120 <nakashi120@gmail.com>
2021-06-18 23:59:50 +0900

    Created Header

--- code/react_web_front/src/components/organisms/layout/Header.tsx
+++ code/react_web_front/src/components/organisms/layout/Header.tsx
@@ -1,7 +1,18 @@
-import { memo, VFC } from "react"
+/* eslint-disable react-hooks/exhaustive-deps */
+import { memo, useCallback, VFC } from "react"
 import { Box, Flex, Heading, Link } from "@chakra-ui/react"
+import { AiOutlineCamera } from "react-icons/ai"
+import { useHistory } from "react-router-dom"
 
 export const Header: VFC = memo(() => {
+  const history = useHistory()
+
+  const onClickHome = useCallback(() => history.push("/home"), [])
+  const onClickMyProfile = useCallback(
+    () => history.push("/home/myprofile"),
+    []
+  )
+
   return (
     <Flex
       as="nav"
@@ -11,19 +22,27 @@ export const Header: VFC = memo(() => {
       justify="space-between"
       padding={{ base: 3, md: 5 }}
     >
-      <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
-        <Heading as="h1" fontSize={{ base: "md", md: "lg" }} pr={3}>
+      <Flex
+        align="center"
+        as="a"
+        mr={8}
+        _hover={{ cursor: "pointer" }}
+        onClick={onClickHome}
+      >
+        <Heading as="h1" fontSize={{ base: "md", md: "lg" }} pr={7}>
           Aritistic Child
         </Heading>
         <Flex align="center" fontSize="sm" display="flex">
           <Box pr={4}>
-            <Link>New Post</Link>
+            <AiOutlineCamera style={{ fontSize: 30 }} />
           </Box>
         </Flex>
       </Flex>
       <Flex align="center" fontSize="sm" display="flex" pr={3}>
         <Box>
-          <Link pr={5}>Profile</Link>
+          <Link pr={5} onClick={onClickMyProfile}>
+            Profile
+          </Link>
         </Box>
         <Link>Logout</Link>
       </Flex>
