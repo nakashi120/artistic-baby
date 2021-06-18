4cabb40d1be48e504ff466666aa80ad2fd03eb5d
nakashi120 <nakashi120@gmail.com>
2021-06-19 00:23:58 +0900

    Created Login page

--- code/react_web_front/src/components/pages/Login.tsx
+++ code/react_web_front/src/components/pages/Login.tsx
@@ -1,5 +1,26 @@
-import { memo, VFC } from "react"
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react"
+import { ChangeEvent, memo, useState, VFC } from "react"
+import { PrimaryButton } from "../atoms/button/PrimaryButton"
 
 export const Login: VFC = memo(() => {
-  return <p>ログインページです。</p>
+  const [email, setEmail] = useState("")
+
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) =>
+    setEmail(e.target.value)
+
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          Artistic Children
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="Email" value={email} onChange={onChangeEmail} />
+          <Input placeholder="password" />
+          <PrimaryButton>ログイン</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  )
 })
