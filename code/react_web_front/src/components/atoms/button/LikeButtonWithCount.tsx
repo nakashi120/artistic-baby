673af750d4984c32a473fc02261629cf0892e78c
nakashi120 <nakashi120@gmail.com>
2021-06-20 00:38:01 +0900

    Added Like Button Function

---
+++ code/react_web_front/src/components/atoms/button/LikeButtonWithCount.tsx
@@ -0,0 +1,55 @@
+import type { HTMLChakraProps } from "@chakra-ui/react"
+import { Box, chakra, Icon, Text, Tooltip } from "@chakra-ui/react"
+import type { HTMLMotionProps } from "framer-motion"
+import { motion, useAnimation } from "framer-motion"
+import type { VFC } from "react"
+import { useState } from "react"
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai"
+
+export type LikeButtonWithCountProps = {
+  count: number
+  isLiked: boolean
+}
+
+const LikeButtonWithCount: VFC<LikeButtonWithCountProps> = (
+  props: LikeButtonWithCountProps
+) => {
+  const [isLike, setIsLike] = useState(false)
+  const controls = useAnimation()
+
+  const toggleLike = async () => {
+    await setIsLike(!isLike)
+    controls.start({
+      scale: [0.9, 1.1, 1.2, 1.2, 1],
+      color: ["#FFF5F5", "#FED7D7", "#FEB2B2", "#FC8181", "#F56565"],
+    })
+  }
+
+  type Merge<P, T> = Omit<P, keyof T> & T
+  type MotionBoxProps = Merge<HTMLChakraProps<"div">, HTMLMotionProps<"div">>
+
+  const MotionBox: React.FC<MotionBoxProps> = motion(chakra.div)
+
+  return (
+    <Box display="flex" alignItems="center" color="gray.500">
+      <Tooltip label="いいね！" bg="gray.400" fontSize="11px">
+        <MotionBox
+          cursor="pointer"
+          onClick={toggleLike}
+          animate={controls}
+          transition={{ duration: 0.2 }}
+        >
+          <Icon
+            as={isLike ? AiFillHeart : AiOutlineHeart}
+            mr="2.5"
+            fontSize="22px"
+            color={isLike ? "red.400" : ""}
+          />
+        </MotionBox>
+      </Tooltip>
+      <Text>{props.count}</Text>
+    </Box>
+  )
+}
+
+export { LikeButtonWithCount }
---
+++ code/react_web_front/src/components/molecules/AvatarAndName.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Avatar, Center, Flex, Text } from "@chakra-ui/react"
+import { memo, VFC } from "react"
+
+type Props = {
+  name: string
+  src: string
+  size: string
+}
+
+export const AvatarAndName: VFC<Props> = memo((props) => {
+  const { name, src, size } = props
+  return (
+    <Flex textAlign="center">
+      <Center mr={3}>
+        <Avatar
+          name={name}
+          src={src}
+          size={size}
+          _hover={{ cursor: "pointer", opacity: 0.8 }}
+        />
+      </Center>
+      <Center>
+        <Text fontSize="15px">{name}</Text>
+      </Center>
+    </Flex>
+  )
+})
