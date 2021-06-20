22fab8a0874b768b24d3bf8145d53b1db8915ccf
nakashi120 <nakashi120@gmail.com>
2021-06-20 12:05:23 +0900

    Created Post Card component

---
+++ code/react_web_front/src/components/organisms/post/PostCard.tsx
@@ -0,0 +1,60 @@
+import {
+  Avatar,
+  Box,
+  Divider,
+  Flex,
+  HStack,
+  Image,
+  Input,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react"
+import { memo, VFC } from "react"
+import { LikeButtonWithCount } from "../../atoms/button/LikeButtonWithCount"
+import { AvatarAndName } from "../../molecules/AvatarAndName"
+
+type Props = {
+  avatarName: string
+  avatarImageSrc: string
+  postImageSrc: string
+}
+
+export const PostCard: VFC<Props> = memo((props) => {
+  const { avatarName, avatarImageSrc, postImageSrc } = props
+
+  return (
+    <Box w="330px" h="550px" bg="white" borderRadius="10px" shadow="md" p={5}>
+      <Stack textAlign="left">
+        <AvatarAndName name={avatarName} src={avatarImageSrc} size="sm" />
+        <Image
+          boxSize="300px"
+          src={postImageSrc}
+          alt="Post Image"
+          m="auto"
+          _hover={{ cursor: "pointer", opacity: 0.8 }}
+        />
+        <HStack>
+          <LikeButtonWithCount count={10} isLiked={false} />
+          <AvatarAndName name={avatarName} src={avatarImageSrc} size="sm" />
+          <Text>Yeah!!!</Text>
+        </HStack>
+        <Divider orientation="horizontal" />
+        {/* Avatars who pushed like button */}
+        <HStack>
+          <Avatar size="xs" />
+          <Avatar size="xs" />
+          <Avatar size="xs" />
+          <Avatar size="xs" />
+        </HStack>
+        <Divider orientation="horizontal" />
+        <Flex>
+          <Input placeholder="add comment..." size="sm" variant="unstyled" />
+          <Link fontSize="xs" color="teal.500">
+            Post
+          </Link>
+        </Flex>
+      </Stack>
+    </Box>
+  )
+})
--- code/react_web_front/src/components/pages/Home.tsx
+++ code/react_web_front/src/components/pages/Home.tsx
@@ -1,20 +1,7 @@
-import {
-  Avatar,
-  Box,
-  Divider,
-  Flex,
-  HStack,
-  Image,
-  Input,
-  Link,
-  Stack,
-  Text,
-  Wrap,
-  WrapItem,
-} from "@chakra-ui/react"
+import { Wrap, WrapItem } from "@chakra-ui/react"
 import { memo, VFC } from "react"
-import { AvatarAndName } from "../molecules/AvatarAndName"
-import { LikeButtonWithCount } from "../atoms/button/LikeButtonWithCount"
+
+import { PostCard } from "../organisms/post/PostCard"
 
 export const Home: VFC = memo(() => {
   return (
@@ -22,57 +9,11 @@ export const Home: VFC = memo(() => {
       <Wrap p={{ base: 4, md: 10 }}>
         {[...Array(10)].map(() => (
           <WrapItem>
-            <Box
-              w="330px"
-              h="550px"
-              bg="white"
-              borderRadius="10px"
-              shadow="md"
-              p={5}
-            >
-              <Stack textAlign="left">
-                <AvatarAndName
-                  name="Nakashi"
-                  src="https://bit.ly/dan-abramov"
-                  size="sm"
-                />
-                <Image
-                  boxSize="300px"
-                  src="https://source.unsplash.com/random"
-                  alt="Post Image"
-                  m="auto"
-                  _hover={{ cursor: "pointer", opacity: 0.8 }}
-                />
-                <HStack>
-                  <LikeButtonWithCount count={10} isLiked={false} />
-                  <AvatarAndName
-                    name="Nakashi"
-                    src="https://bit.ly/dan-abramov"
-                    size="sm"
-                  />
-                  <Text>Yeah!!!</Text>
-                </HStack>
-                <Divider orientation="horizontal" />
-                {/* Avatars who pushed like button */}
-                <HStack>
-                  <Avatar size="xs" />
-                  <Avatar size="xs" />
-                  <Avatar size="xs" />
-                  <Avatar size="xs" />
-                </HStack>
-                <Divider orientation="horizontal" />
-                <Flex>
-                  <Input
-                    placeholder="add comment..."
-                    size="sm"
-                    variant="unstyled"
-                  />
-                  <Link fontSize="xs" color="teal.500">
-                    Post
-                  </Link>
-                </Flex>
-              </Stack>
-            </Box>
+            <PostCard
+              avatarName="Nakashi"
+              avatarImageSrc="https://bit.ly/dan-abramov"
+              postImageSrc="https://source.unsplash.com/random"
+            />
           </WrapItem>
         ))}
       </Wrap>
